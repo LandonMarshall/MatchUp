4745a8b987465b45129343ca429ecc53c1b60562
Landon <landonmarshall1997@gmail.com>
2023-05-17 14:12:34 -0700

    first frontend commit

--- front-end/src/app/layout.tsx
+++ front-end/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
+import { Outfit } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const outfit = Outfit({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Header />
+      <body className={outfit.className}>{children}</body>
     </html>
   )
 }
